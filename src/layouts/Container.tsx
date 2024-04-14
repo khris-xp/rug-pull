74686c3efcbd093b4d72088d2c670fd2de5cccb8
khris-xp <khrisbharmmano@gmail.com>
2024-04-14 16:03:18 +0700

    feat(component): bento card compoent

---
+++ src/layouts/Container.tsx
@@ -0,0 +1,32 @@
+interface IVariant {
+  display?: string;
+  justifyContent?: string;
+  margin?: string;
+  container?: boolean;
+}
+
+interface ContainerProps {
+  variant: IVariant;
+  children: React.ReactNode;
+}
+
+export default function Container({ variant, children }: ContainerProps) {
+  const defaultVariant: IVariant = {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 'mx-auto',
+    container: true,
+  };
+
+  const mergedVariant = { ...defaultVariant, ...variant };
+
+  return (
+    <div
+      className={`${mergedVariant.display} ${mergedVariant.justifyContent} ${
+        mergedVariant.margin
+      } ${mergedVariant.container && 'container'}`}
+    >
+      {children}
+    </div>
+  );
+}
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,5 +1,7 @@
 import Banner from '@/components/Banner/Banner';
+import BentoCard from '@/components/Card/BentoCard';
 import ProductCard from '@/components/Card/ProductCard';
+import Container from '@/layouts/Container';
 import { Fragment } from 'react/jsx-runtime';
 
 export default function HomePage() {
@@ -11,6 +13,16 @@ export default function HomePage() {
         backgroundImage='https://canazeegames.ca/cdn/shop/files/game-play-DSCF3536.jpg?v=1653398205&width=3200'
         height='75vh'
       />
+      <Container
+        variant={{
+          display: 'flex',
+          justifyContent: 'center',
+          container: true,
+          margin: 'mx-auto',
+        }}
+      >
+        <BentoCard />
+      </Container>
       <div className='flex justify-center container mx-auto'>
         <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3'>
           <ProductCard />
